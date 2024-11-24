cbbadb056b5e464957cce9d2c35efc2be93061f1
gz <adriangonzacc40@gmail.com>
2024-11-24 16:37:11 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<?php
+include_once "includes/templates/header.php";
+?>
+
+  <section class="seccion contenedor">
+    <h2>La mejor conferencia de diseño web en español</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+      assumenda officiis pariatur exercitationem debitis inventore adipisci
+      magni sequi, eligendi porro sed dolor a nisi neque maiores libero non
+      ipsa totam!
+    </p>
+  </section> <!-- seccion contenedor -->
+
+  <section class="programa">
+    <div class="contenedor-video">
+      <video autoplay loop poster="img/bg-talleres.jpg">
+        <source src="video/video.mp4" type="video/mp4">
+        <source src="video/video.webm" type="video/webm">
+        <source src="video/video.ogv" type="video/ogg">
+      </video>
+    </div> <!-- contendedor video -->
+    <div class="contenido-programa">
+      <div class="contenedor">
+        <div class="programa-evento">
+          <h2>Programa de evento</h2>
+          <nav class="menu-programa">
+            <a href="#seminario"><i class="fa fa-university" aria-hidden="true"></i>Seminario</a>
+            <a href="#seminario"><i class="fa fa-comment" aria-hidden="true"></i>Conferencias</a>
+            <a href="#seminario"><i class="fa fa-code" aria-hidden="true"></i>Talleres</a>
+          </nav>
+
+          <div id="seminario" class="info-curso ocultar clearfix">
+            <div class="detalle-evento">
+              <h3>Diseño UI y UX para móviles</h3>
+              <p><i class="fa fa-clock-o" aria-hidden="true"></i>10:00</p>
+              <p><i class="fa fa-calendar" aria-hidden="true"></i>2024-12-13</p>
+              <p><i class="fa fa-user" aria-hidden="true"></i>Susan Sanchez</p>
+            </div>
+          </div>
+          <div class="detalle-evento">
+            <h3>Angular</h3>
+            <p><i class="fa fa-clock-o" aria-hidden="true"></i>10:00</p>
+            <p><i class="fa fa-calendar" aria-hidden="true"></i>2024-12-13</p>
+            <p><i class="fa fa-user" aria-hidden="true"></i>Susan Sanchez</p>
+          </div>
+          <div class="detalle-evento">
+            <h3>Angular</h3>
+            <p><i class="fa fa-clock-o" aria-hidden="true"></i>10:00</p>
+            <p><i class="fa fa-calendar" aria-hidden="true"></i>2024-12-13</p>
+            <p><i class="fa fa-user" aria-hidden="true"></i>Susan Sanchez</p>
+          </div>
+          <div class="detalle-evento">
+            <h3>Diseño UI y UX para móviles</h3>
+            <p><i class="fa fa-clock-o" aria-hidden="true"></i>10:00</p>
+            <p><i class="fa fa-calendar" aria-hidden="true"></i>2024-12-13</p>
+            <p><i class="fa fa-user" aria-hidden="true"></i>Susan Sanchez</p>
+          </div>
+        </div>
+      </div> <!--seccion programa-evento-->
+    </div> <!--div contendor-->
+    </div>
+    </div> <!--div contenido-programa-->
+  </section> <!-- seccion programa -->
+
+  <section class="invitados contenedor seccion">
+    <h2> Nuestros invitados</h2>
+    <ul class="lista-invitados clearfix">
+      <li>
+        <div class="invitado">
+          <a href="#invitado1" class="invitado-info">
+            <img src="img/invitado1.jpg" alt="invitado1">
+          </a>
+          <p>Samuel Pérez</p>
+        </div> <!--div invitado-->
+
+      </li>
+      <div style="display: none;">
+        <div class="invitado-info" id="invitado1">
+          <h2>Samuel</h2>
+          <img src="img/invitado1.jpg" alt="invitado1">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, quisquam! Dignissimos labore non iste ut ducimus, commodi animi architecto dolores
+            provident laudantium quos accusamus soluta, nesciunt officia iure eligendi fuga!</p>
+        </div> <!-- div invitado-info -->
+      </div> <!-- div style -->
+
+      <li>
+        <div class="invitado">
+          <a href="#invitado2" class="invitado-info">
+            <img src="img/invitado2.jpg" alt="invitado2">
+          </a>
+          <p>Esmeralda Ignacio</p>
+        </div> <!--div invitado-->
+
+      </li>
+      <div style="display: none;">
+        <div class="invitado-info" id="invitado2">
+          <h2>Esmeralda</h2>
+          <img src="img/invitado2.jpg" alt="invitado2">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, quisquam! Dignissimos labore non iste ut ducimus, commodi animi architecto dolores
+            provident laudantium quos accusamus soluta, nesciunt officia iure eligendi fuga!</p>
+        </div> <!-- div invitado-info -->
+      </div> <!-- div style -->
+
+      <li>
+        <div class="invitado">
+          <a href="#invitado3" class="invitado-info">
+            <img src="img/invitado3.jpg" alt="invitado3">
+          </a>
+          <p>Ulises Espinoza</p>
+        </div> <!--div invitado-->
+
+      </li>
+      <div style="display: none;">
+        <div class="invitado-info" id="invitado3">
+          <h2>Ulises</h2>
+          <img src="img/invitado3.jpg" alt="invitado3">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, quisquam! Dignissimos labore non iste ut ducimus, commodi animi architecto dolores
+            provident laudantium quos accusamus soluta, nesciunt officia iure eligendi fuga!</p>
+        </div> <!-- div invitado-info -->
+      </div> <!-- div style -->
+
+      <li>
+        <div class="invitado">
+          <a href="#invitado4" class="invitado-info">
+            <img src="img/invitado4.jpg" alt="invitado4">
+          </a>
+          <p>Asuncion Martinez</p>
+        </div> <!--div invitado-->
+
+      </li>
+      <div style="display: none;">
+        <div class="invitado-info" id="invitado4">
+          <h2>Asuncion</h2>
+          <img src="img/invitado4.jpg" alt="invitado4">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, quisquam! Dignissimos labore non iste ut ducimus, commodi animi architecto dolores
+            provident laudantium quos accusamus soluta, nesciunt officia iure eligendi fuga!</p>
+        </div> <!-- div invitado-info -->
+      </div> <!-- div style -->
+
+      <li>
+        <div class="invitado">
+          <a href="#invitado5" class="invitado-info">
+            <img src="img/invitado5.jpg" alt="invitado5">
+          </a>
+          <p>Alejandro Velazquez</p>
+        </div> <!--div invitado-->
+
+      </li>
+      <div style="display: none;">
+        <div class="invitado-info" id="invitado5">
+          <h2>Alejandro</h2>
+          <img src="img/invitado5.jpg" alt="invitado5">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, quisquam! Dignissimos labore non iste ut ducimus, commodi animi architecto dolores
+            provident laudantium quos accusamus soluta, nesciunt officia iure eligendi fuga!</p>
+        </div> <!-- div invitado-info -->
+      </div> <!-- div style -->
+
+      <li>
+        <div class="invitado">
+          <a href="#invitado6" class="invitado-info">
+            <img src="img/invitado6.jpg" alt="invitado6">
+          </a>
+          <p>Sarahi Ramirez</p>
+        </div> <!--div invitado-->
+
+      </li>
+      <div style="display: none;">
+        <div class="invitado-info" id="invitado6">
+          <h2>Sarahi</h2>
+          <img src="img/invitado6.jpg" alt="invitado6 ">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus, quisquam! Dignissimos labore non iste ut ducimus, commodi animi architecto dolores
+            provident laudantium quos accusamus soluta, nesciunt officia iure eligendi fuga!</p>
+        </div> <!-- div invitado-info -->
+      </div> <!-- div style -->
+
+    </ul> <!-- UL -->
+  </section> <!-- seccion invitados contenedor -->
+
+  <div class="contador parallax">
+    <div class="contenedor">
+      <ul class="resumen-evento clearfix">
+        <li>
+          <p class="numero">0</p>Invitados
+        </li>
+        <li>
+          <p class="numero">0</p>Talleres
+        </li>
+        <li>
+          <p class="numero">0</p>Dias
+        </li>
+        <li>
+          <p class="numero">0</p>Conferencias
+        </li>
+      </ul>
+    </div> <!-- div contenedor -->
+  </div> <!-- div contador parallax -->
+
+  <section class="precios seccion">
+    <h2>Precios</h2>
+    <div class="contenedor">
+      <ul class="lista-precios clearfix">
+        <li>
+          <div class="tabla-precio">
+            <h3>Pase por Día</h3>
+            <p class="numero">$30</p>
+            <ul>
+              <li>Bocadillos gratis</li>
+              <li>Todas las conferencias</li>
+              <li>Todos los talleres</li>
+            </ul>
+            <a href="#" class="button hollow">Comprar</a>
+          </div> <!--div tabla-precios-->
+        </li>
+        <li>
+          <div class="tabla-precio">
+            <h3>Pase por dos dias</h3>
+            <p class="numero">$45</p>
+            <ul>
+              <li>Bocadillos gratis</li>
+              <li>Todas las conferencias</li>
+              <li>Todos los talleres</li>
+            </ul>
+            <a href="#" class="button hollow">Comprar</a>
+          </div> <!--div tabla-precios-->
+        </li>
+        <li>
+          <div class="tabla-precio">
+            <h3>Todos los dias</h3>
+            <p class="numero">$50</p>
+            <ul>
+              <li>Bocadillos gratis</li>
+              <li>Todas las conferencias</li>
+              <li>Todos los talleres</li>
+            </ul>
+            <a href="#" class="button hollow">Comprar</a>
+          </div> <!--div tabla-precios-->
+        </li>
+    </div> <!--div tabla-precios-->
+    </li>
+    </ul> <!--lista-precios clearfix-->
+    </div> <!--div contenedor-->
+  </section> <!--seccion precios-->
+
+  <section class="seccion">
+    <h2>Testimoniales</h2>
+    <div class="testimoniales contenedor clearfix">
+      <div class="testimonial">
+        <blockquote>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Animi placeat minus deleniti eligendi, dolor, facilis ad vitae voluptas
+            cumque necessitatibus iure qui autem, ut optio voluptatem earum. Mollitia, tempore animi.</p>
+          <p>
+          <footer class="info-testimonial clearfix">
+            <img src="img/testimonial.jpg" alt="imagen testimonial">
+            <cite>Oswald Aponte Escobedo <span>Diseñador en @Prisma</span></cite>
+          </footer>
+          </p>
+        </blockquote>
+      </div> <!--div testimonial-->
+      <div class="testimonial">
+        <blockquote>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Animi placeat minus deleniti eligendi, dolor, facilis ad vitae voluptas
+            cumque necessitatibus iure qui autem, ut optio voluptatem earum. Mollitia, tempore animi.</p>
+          <p>
+          <footer class="info-testimonial clearfix">
+            <img src="img/testimonial.jpg" alt="imagen testimonial">
+            <cite>Oswald Aponte Escobedo <span>Diseñador en @Prisma</span></cite>
+          </footer>
+          </p>
+        </blockquote>
+      </div> <!--div testimonial-->
+      <div class="testimonial">
+        <blockquote>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Animi placeat minus deleniti eligendi, dolor, facilis ad vitae voluptas
+            cumque necessitatibus iure qui autem, ut optio voluptatem earum. Mollitia, tempore animi.</p>
+          <p>
+          <footer class="info-testimonial clearfix">
+            <img src="img/testimonial.jpg" alt="imagen testimonial">
+            <cite>Oswald Aponte Escobedo <span>Diseñador en @Prisma</span></cite>
+          </footer>
+          </p>
+        </blockquote>
+      </div> <!--div testimonial-->
+    </div> <!--div testimoniales contendor clearfix-->
+  </section> <!--seccion-->
+
+  <div class="newsletter parallax">
+    <div class="contenido contenedor">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Adipisci perspiciatis officiis explicabo sit animi porro recusandae incidunt totam,
+        cupiditate ab. Accusamus totam nesciunt beatae labore, enim temporibus. Est, at dolores?</p>
+      <h3>VWepCamp</h3>
+      <a href="#mc_embed_signup" class="boton_newsletter button transparente">registro</a>
+    </div> <!--contenido contenedor-->
+  </div><!--newsletter parallax-->
+
+  <section class="seccion">
+    <h2>Faltan</h2>
+    <div class="cuenta-regresiva contenedor">
+      <ul class="clearfix">
+        <li>
+          <p id="dias" class="numero">0</p>dias
+        </li>
+        <li>
+          <p id="horas" class="numero">0</p>horas
+        </li>
+        <li>
+          <p id="minutos" class="numero">0</p>minutos
+        </li>
+        <li>
+          <p id="segundos" class="numero">0</p>segundos
+        </li>
+      </ul> <!--clearfix-->
+    </div> <!--cuenta-regresiva-->
+  </section> <!--seccion-->
+
+<?php
+include_once "includes/templates/footer.php";
+?>
